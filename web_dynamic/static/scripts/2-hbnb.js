c5315c0c0c4189b2f61725d853640fbab9bea26e
Oluwasegun Coker <cokeroluwasegun@outlook.com>
2022-04-23 14:36:48 +0100

    Created Taske 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$('document').ready(() => {
+  $.get('http://localhost:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+
+$(function () {
+        let amenities = {};
+	$('input[type="checked"]').change(function () {
+		if ($(this).is(':checked')) {
+			amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+		} else {
+			delete amenities[$(this).attr('data-id')];
+		}
+		$('.amenities H4').text()
+	});
+});
