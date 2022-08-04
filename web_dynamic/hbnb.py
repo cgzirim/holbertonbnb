dcc8126cb381422994be61deb7473d1d1cb73725
Chigozirim <iamokwukwe@gmail.com>
2022-08-04 03:04:22 +0100

    Rename files, remove unnecessary task files

--- web_dynamic/hbnb.py
+++ web_dynamic/hbnb.py
@@ -8,10 +8,10 @@ from models.place import Place
 import uuid
 from os import environ
 from flask import Flask, render_template
-app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
+
+app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
 
 @app.teardown_appcontext
 def close_db(error):
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -37,13 +37,13 @@ def hbnb():
 
     cache_id = str(uuid.uuid4())
 
-    return render_template('0-hbnb.html',
+    return render_template('index.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
